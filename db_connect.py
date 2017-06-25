117c12a8f56755b1e5cf6d8c4a15bab2f9bd2a0d
gabee1987 <gabee1987@gmail.com>
2017-06-25 18:10:16 +0200

    Made heroku related changes

--- db_connect.py
+++ db_connect.py
@@ -4,8 +4,21 @@
 '''
 
 
+import os
 import psycopg2
+import urllib
 from local_config import *
+
+
+urllib.parse.uses_netloc.append('postgres')
+url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
+connection = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
 
 
 def connect_db(connect_data):
